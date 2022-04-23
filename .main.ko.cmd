3eea2df9e93021ac5e8e8d04456c6d920f8756e8
2021H1400158p <99498248+2021H1400158p@users.noreply.github.com>
2022-04-23 21:36:25 +0530

    Add files via upload

---
+++ .main.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/lab/Videos/abc/DD/main.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/lab/Videos/abc/DD/main.ko /home/lab/Videos/abc/DD/main.o /home/lab/Videos/abc/DD/main.mod.o ;  true
